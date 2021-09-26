e9de11026ee7af42105aa2ebc3730b0d24f3b814
Alan Dev <ingescobedoalan4@gmail.com>
2021-09-26 15:32:37 -0500

    Web Server completed

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+let hbs = require("hbs");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+//HandleBars
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+//Servir contenido estatico
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "Alan",
+    title: "Learning HBS",
+  });
+});
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    name: "Alan",
+    title: "Learning HBS",
+  });
+});
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    name: "Alan",
+    title: "Learning HBS",
+  });
+});
+
+app.listen(PORT);
