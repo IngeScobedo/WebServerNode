f2b4cd53a12c266b5d52e4c8906e0640cec57707
Alan Dev <ingescobedoalan4@gmail.com>
2021-09-26 20:06:17 -0500

    React Deploy

--- app.js
+++ app.js
@@ -11,23 +11,28 @@ hbs.registerPartials(__dirname + "/views/partials");
 //Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    name: "Alan",
-    title: "Learning HBS",
-  });
-});
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    name: "Alan",
-    title: "Learning HBS",
-  });
-});
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    name: "Alan",
-    title: "Learning HBS",
-  });
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     name: "Alan",
+//     title: "Learning HBS",
+//   });
+// });
+// app.get("/generic", (req, res) => {
+//   res.render("generic", {
+//     name: "Alan",
+//     title: "Learning HBS",
+//   });
+// });
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     name: "Alan",
+//     title: "Learning HBS",
+//   });
+// });
+app.get("*", (req, res) => {
+  res.sendFile( __dirname + "/public/index.html")
 });
 
-app.listen(PORT);
+app.listen(PORT, ()=>{
+  console.log(`Server running on port: ${PORT}`);
+} );
